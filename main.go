cc14b7add608e2fa1382513e27b46e575b7f57c4
Asael Tobar <2023158061@ub.edu.bz>
2025-03-30 01:03:39 -0600

    Added -start-port and -end-port flags

--- main.go
+++ main.go
@@ -43,11 +43,29 @@ func main() {
 
 	target := flag.String("target", "", "IP address or hostname to scan (required)")
 
+	startPort := flag.String("-start-port", "1", "Enter a number from 0 to 65535")
+
+	endPort := flag.String("-end-port", "1024", "Enter a number from 0 to 65535")
+
 	flag.Parse()
 
 	if *target == "" {
 		fmt.Println("Error: -target flag is required")
 		flag.Usage()
+		os.Exit(1)
+	}
+
+	startPortNumber, err := strconv.Atoi(*startPort)
+
+	if err != nil || startPortNumber < 0 || startPortNumber > 65535 {
+		fmt.Println("Error: Invalid port range. Ports must be between 0 and 65535.")
+		os.Exit(1)
+	}
+
+	lastPortNumber, err := strconv.Atoi(*endPort)
+
+	if err != nil || lastPortNumber < 0 || startPortNumber > 65535 {
+		fmt.Println("Error: Invalid port range. Ports must be between 0 and 65535.")
 		os.Exit(1)
 	}
 
